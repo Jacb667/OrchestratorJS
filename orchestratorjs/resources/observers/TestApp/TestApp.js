c4cb1291c6e0b37c0d8971fca6f374e096bbc71a
jacb667 <joscorbe@hotmail.com>
2014-11-10 21:14:08 +0100

    TestApp with some tests

--- orchestratorjs/resources/observers/TestApp/TestApp.js
+++ orchestratorjs/resources/observers/TestApp/TestApp.js
@@ -1,39 +1,57 @@
-var httprequest = require( "request" );
-var socket = require('socket.io-client').connect('http://192.168.1.180:9002');
+var httprequest = require( '../../tools.js' ).httprequest;
+var pubsub      = require( '../../tools.js' ).pubsub();
+var tools       = require( '../../tools.js' );
+var Fiber       = require( 'fibers' );
 
-socket.on( 'online', function ( contextValue, deviceIdentity ) {
-  if ( contextValue != true || deviceIdentity != 'Jose@Bq' )
-    return;
+
+
+module.exports = {
   
-  var params = {};
-  params[ 'actionName' ] = 'MakeCoffee';
-  params[ 'parameters' ] = [ 'device:Jose@Bq', 'device:Jose@Bq' ];
+  // Define here settings that the app needs from user
+  // ( asked from the user s/he starts the app )
+  settings: {  },
 
-  httprequest( {
-    uri: 'http://192.168.1.180:9000/api/1/actioninstance',
-    method: "POST",
-    form: params
-  }, function( error, response, body ) {
-    console.log( 'body: '+body );
-  } );
-  
-});
+  // Define your action triggering logic here
+  logic: function()
+  {
+    pubsub.on( 'online', function( contextValue, deviceIdentity ) {
+
+      // runs every ten seconds until the app gets stopped
+
+        console.log( 'tick' );
+      	console.log( deviceIdentity );
+        
+              var paramsPhoneDialog = {
+                actionName: 'ShowVisitingCard',
+                parameters: [ 'device:' + deviceIdentity, "holaa", "aass", "asgasg", "asgage", "aaa", "" ]
+              };
+
+              httprequest( {
+                uri: '/api/1/actioninstance',
+                method: "POST",
+                form: paramsPhoneDialog
+              }, function( error, response, body ) {} );
 
 
-socket.on( 'coffeeReady', function ( contextValue, deviceIdentity ) {
-  if ( contextValue != true )
-    return;
-  
-  var params = {};
-  params[ 'actionName' ] = 'InviteForCoffee';
-  params[ 'parameters' ] = [ 'device:Jose@Bq', 'device:Jose@Bq' ];
+    } );
+    
 
-  httprequest( {
-    uri: 'http://192.168.1.180:9000/api/1/actioninstance',
-    method: "POST",
-    form: params
-  }, function( error, response, body ) {
-    console.log( 'body: '+body );
-  } );
-  
-});
+  }
+};
+
+
+
+/*var request = require("request")
+var url = "http://192.168.1.180:9000/api/1/devices"
+
+request({
+	url: url,
+	json: true
+	}, function (error, response, body) {
+
+		if (!error && response.statusCode === 200)
+		{
+			//console.log(body) // Print the json response
+			console.log("Device id = " + body.devices[3]["metadata"]["proximityDevices"])
+		}
+})*/
